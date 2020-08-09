1f38cb4f9a1c6b7674c3426e22f8c9cb65df8223
m.teshita <masato.t2207@gmail.com>
2020-08-10 00:37:45 +0900

    feat: postsテーブルの作成とマイグレート

--- db/migrate/20200809133118_create_posts.rb
+++ db/migrate/20200809133118_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.references :user, foreign_key: true, null: false
+      t.text :text ,null: false
       t.timestamps
     end
   end
