28f6b0dd96b8891e39f582151e7437e53aae1909
m.teshita <masato.t2207@gmail.com>
2020-08-11 00:04:54 +0900

    feat: ゲストユーザーを生成するメソッドの定義

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com', name: 'ゲストユーザ') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
