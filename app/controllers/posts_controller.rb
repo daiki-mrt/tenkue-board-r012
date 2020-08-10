6fbf7cae64845b038702463dc2413885afb41c13
m.teshita <masato.t2207@gmail.com>
2020-08-10 22:08:16 +0900

    feat: posts/show, edit, update, destroyアクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -12,7 +14,26 @@ class PostsController < ApplicationController
     @post.save ? (redirect_to root_path):(render :new)
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params) ? (redirect_to post_path(@post.id)):(render :edit)
+  end
+
+  def destroy
+    @post.destroy ? (redirect_to root_path):(redirect_to post_path(@post.id))
+  end
+
   private
+
+  def set_post
+    @post = Post.find_by(id: params[:id])
+  end
+
   def post_params
     params.require(:post).permit(
       :text,
