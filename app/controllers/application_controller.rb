2603018884c5287ff1839a71edb639eafdc0d3ef
m.teshita <masato.t2207@gmail.com>
2020-08-11 01:40:30 +0900

    feat: ログイン後に、アクセスしようとした制限ページへリダイレクトする処理を追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :set_host
 
   protected
+  def set_host
+    Rails.application.routes.default_url_options[:host] = request.host_with_port
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
