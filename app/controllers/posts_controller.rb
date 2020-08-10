8ea947b28513385a248285dfb0228cb2e2961c61
m.teshita <masato.t2207@gmail.com>
2020-08-10 16:41:23 +0900

    feat: post/index, new, createアクションを定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,22 @@
 class PostsController < ApplicationController
   def index
-    
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save ? (redirect_to root_path):(render :new)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(
+      :text,
+      :user_id,
+    )
   end
 end
