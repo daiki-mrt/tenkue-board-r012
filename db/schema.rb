690530de22c92804115957fc606447031737d16a
m.teshita <masato.t2207@gmail.com>
2020-08-11 23:20:05 +0900

    fix: commentsテーブルに、post_idがなかったため追加

--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,11 @@ ActiveRecord::Schema.define(version: 2020_08_10_164138) do
 
   create_table "comments", force: :cascade do |t|
     t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
     t.text "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -44,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_164138) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
