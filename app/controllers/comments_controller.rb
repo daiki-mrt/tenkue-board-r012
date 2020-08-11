5f645e9347c917e7d0bdd4d5dbdeec988d83dbb0
m.teshita <masato.t2207@gmail.com>
2020-08-11 23:23:12 +0900

    feat: comment/edit, updateのアクションを定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,14 @@ class CommentsController < ApplicationController
     @comment.save ? (redirect_to post_path(@comment.post_id)):(redirect_to post_path(@comment.post_id), alert: 'コメントを入力してください。')
   end
 
+  def edit
+    redirect_to post_path(@post.id), flash: {comment_id: @comment.id }
+  end
+
+  def update
+    @comment.update(comment_params) ? (redirect_to post_path(@post.id)):(render :edit)
+  end
+
   private
   def set_post
     @post = Post.find_by(id: params[:post_id])
