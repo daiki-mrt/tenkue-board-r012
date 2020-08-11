18311076325883ec006f206b5a7f76693dba871d
m.teshita <masato.t2207@gmail.com>
2020-08-11 23:21:51 +0900

    feat: comments/createアクションの定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :destroy]
+  before_action :set_post, only: [:edit, :update, :destroy]
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save ? (redirect_to post_path(@comment.post_id)):(redirect_to post_path(@comment.post_id), alert: 'コメントを入力してください。')
+  end
+
+  private
+  def set_post
+    @post = Post.find_by(id: params[:post_id])
+  end
+
+  def set_comment
+    @comment = Comment.find_by(id: params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(
+      :text,
+      :user_id,
+      :post_id,
+    )
+  end
 end
