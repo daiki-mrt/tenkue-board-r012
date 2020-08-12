5bae2bbc269e291e565960d4c97371a360a02489
m.teshita <masato.t2207@gmail.com>
2020-08-12 23:14:02 +0900

    feat: commentモデルのスコープを設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user  
   belongs_to :post
   validates :text, presence: true
+
+  scope :desc,      -> {order('created_at DESC')}
+  scope :including, -> {includes(:user)}
 end
