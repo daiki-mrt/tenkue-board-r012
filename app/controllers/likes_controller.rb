17cfc6bcfc627f3d82de2d61b5baf233594c8c75
m.teshita <masato.t2207@gmail.com>
2020-08-12 21:15:25 +0900

    feat: likesコントローラーにcreate/destroyアクションを定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,19 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
   def create
+    like = Like.create(user_id: current_user.id, post_id: @post.id)
+    redirect_to post_path(@post)
   end
 
   def destroy
+    like = Like.find_by(id: params[:id])
+    like.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+  def set_post
+    @post = Post.find_by(id: params[:post_id])
   end
 end
