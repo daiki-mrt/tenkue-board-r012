3be19414b6730bbd2f91b0c2838903b00b22850d
m.teshita <masato.t2207@gmail.com>
2020-08-15 21:29:09 +0900

    feat: Userモデルにバリデーションを追加
    ・名前（name）がないと登録できない
    ・パスワードは8文字以上32文字以下
    ・パスワードは半角英数が1種類ずつ必要
    ・メールアドレスがないと登録できない
    ・メールアドレスは半角英数のみ
    ・メールアドレスは重複登録できない

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  validates :name, presence: true
+  validates :password, {length: { in: 8..32 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]/}}
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }}
+
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
   end
