89a52c2a8b4e1d6cbf1016ddb7d9bda2d597e2e6
m.teshita <masato.t2207@gmail.com>
2020-08-15 22:16:27 +0900

    refactor: インデント等の微修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  validates :text, {length: { maximum: 140 }, presence: true}
 
   scope :desc,      -> {order('created_at DESC')}
   scope :including, -> {includes(:user)}
-
-validates :text, {length: { maximum: 140 }, presence: true}
 end
