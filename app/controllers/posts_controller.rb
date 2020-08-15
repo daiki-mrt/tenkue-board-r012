43716d4bc457b68223aa2be76018383be19c0bf2
m.teshita <masato.t2207@gmail.com>
2020-08-15 22:34:24 +0900

    feat: 投稿一覧のインスタンスにページネートの情報を追記

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.including.desc
+    @posts = Post.including.desc.page(params[:page]).per(5)
   end
 
   def new
