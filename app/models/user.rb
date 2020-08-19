61c3cc9de8d49db8dc48c6d34f58ef724ca28e2b
m.teshita <masato.t2207@gmail.com>
2020-08-19 21:47:44 +0900

    feat: アプリ雛形の状態に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
-
-  validates :name, presence: true
-  validates :password, {length: { in: 8..32 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]/}}
-  before_save { self.email = email.downcase }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, {presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }}
-
-  def already_liked?(post)
-    self.likes.exists?(post_id: post.id)
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,4 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  root "posts#index"
-  resources :posts do
-    resources :comments, only: [:new, :create, :edit, :update, :destroy]
-    resources :likes, only: [:create, :destroy]
-  end
 end
