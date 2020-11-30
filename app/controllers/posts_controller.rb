84657aec5f3505a9d0f468f1c30bef708885de96
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-30 10:02:33 +0900

    new, createアクションを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,22 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content).merge(user_id: current_user.id)
+  end
 end
