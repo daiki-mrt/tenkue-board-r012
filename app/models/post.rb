20812aa4a7ac84ac1618375c7ee0adc7bce951ba
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-30 10:03:00 +0900

    エラーメッセージの表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  validates :content, presence: true
-  validates :content, length: { maximum: 140 }
+  validates :content, presence: { message: "を入力してください" }
+  validates :content, length: { maximum: 140, too_long: "は140文字以内で入力してください" }
 end
