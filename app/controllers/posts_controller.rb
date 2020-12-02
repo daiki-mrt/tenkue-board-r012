3e1512f7c51097d16bc629fdd139ea2f7ddd094d
Kota Kamiyama <k.kamikami1211@gmail.com>
2020-12-02 18:46:46 +0900

    posts.controller.rbのset_postとeditアクションに記述したif文を合わせてuse_rimitメソッドを作成(掲示板を編集・削除できるのは投稿者のみにするため)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: :index
-  before_action :set_post, only: [:edit, :update, :destroy]
+  before_action :use_rimit, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.eager_load(:user).all
@@ -20,9 +20,6 @@ class PostsController < ApplicationController
   end
 
   def edit
-    if current_user.id != @post.user_id
-      redirect_to root_path
-    end
   end
 
   def update
@@ -43,7 +40,11 @@ class PostsController < ApplicationController
     params.require(:post).permit(:content).merge(user_id: current_user.id)
   end
 
-  def set_post
+  def use_rimit
     @post = Post.find(params[:id])
+
+    if current_user.id != @post.user_id
+      redirect_to root_path
+    end
   end
 end
