79e1833b2412b422447abb0dcf8f4e91ceb2ac77
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-12-02 15:23:12 +0900

    ゲストユーザの検索/作成をuserモデルに移動

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,10 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   def new_guest
-    user = User.find_or_create_by!(email: 'guest@example.com', name: 'ゲストユーザ') do |user|
-      user.password = SecureRandom.urlsafe_base64
-      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
-    end
+    user = User.guest
     sign_in user
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
