1eee37e433dc9ac3de6312a444ba0853b3335c4f
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-12-02 16:56:43 +0900

    showアクション追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.eager_load(:user).all
@@ -16,6 +16,10 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   private
