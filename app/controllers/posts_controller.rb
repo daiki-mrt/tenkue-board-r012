6e8af5743c9b14355da0b5cb24aca1096356267c
Kota Kamiyama <k.kamikami1211@gmail.com>
2020-12-02 17:34:36 +0900

    posts_controller.rbにdestroyアクションを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,11 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy!
+    redirect_to root_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:content).merge(user_id: current_user.id)
