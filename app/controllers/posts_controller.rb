94f6ed902e1ad146cc14afa2d04a8cbcb1e0950c
Kota Kamiyama <k.kamikami1211@gmail.com>
2020-12-06 21:07:52 +0900

    add: N+1問題を解消

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comment = Comment.new
-    @comments = Comment.all.where(post_id: params[:id])
+    @comments = Comment.eager_load(:user).where(post_id: params[:id])
   end
 
   def edit
