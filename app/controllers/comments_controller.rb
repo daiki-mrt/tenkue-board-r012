7c349e0513d2278d799899469c058775fc63b9c2
Kota Kamiyama <k.kamikami1211@gmail.com>
2020-12-06 18:12:53 +0900

    add: createアクションを作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    redirect_to root_path
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:comment,:post_id).merge(user_id: current_user.id)
+    end
 end
