c374f403e50a91646304456caf8581e78f9e2865
Kota Kamiyama <k.kamikami1211@gmail.com>
2020-12-07 22:38:08 +0900

    fix: posts_controller.rbのindexアクションのインスタンス変数を変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :access_limit, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.eager_load(:user,:comments).all
+    @posts = Post.eager_load(:user).all
   end
 
   def new
