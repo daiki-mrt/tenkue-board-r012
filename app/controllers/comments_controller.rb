916ddcba4621ba7f92b9007c0fcb275410a91d04
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-12-08 19:47:18 +0900

    comment_params post_idの取得方法を変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -29,7 +29,11 @@ class CommentsController < ApplicationController
 
   private
 
+    # def comment_params
+    #   params.require(:comment).permit(:comment,:post_id).merge(user_id: current_user.id)
+    # end
+
     def comment_params
-      params.require(:comment).permit(:comment,:post_id).merge(user_id: current_user.id)
+      params.require(:comment).permit(:comment).merge(user_id: current_user.id, post_id: params[:post_id])
     end
 end
