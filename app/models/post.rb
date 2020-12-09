746e795ed6d7b3ffafba44992169d74379dfedd1
Kota Kamiyama <k.kamikami1211@gmail.com>
2020-12-09 17:50:05 +0900

    add: アソシエーションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :content, presence: { message: "を入力してください" }
   validates :content, length: { maximum: 140, too_long: "は140文字以内で入力してください" }
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
-  
+  has_many :likes, dependent: :destroy
+
   def self.guest
     user = User.find_or_create_by!(email: 'guest@example.com', name: 'ゲストユーザ') do |user|
       user.password = SecureRandom.urlsafe_base64
