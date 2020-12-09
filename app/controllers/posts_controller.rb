8d52189be03b2b01a64ecfcc14216b8a7b261f6e
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-12-09 13:42:57 +0900

    postとcommentの表示を降順に変更(追加修正)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :access_limit, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.eager_load(:user).all
+    @posts = Post.eager_load(:user).all.order(created_at: "DESC")
   end
 
   def new
