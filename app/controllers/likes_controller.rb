82f254c70fa91658bed46e75cc40a28cab81f93e
Kota Kamiyama <k.kamikami1211@gmail.com>
2020-12-09 19:49:14 +0900

    add: アクションの作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,15 @@
 class LikesController < ApplicationController
+  def create
+    @like = current_user.likes.build(post_id: params[:post_id])
+    @like.save
+
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    @like.destroy
+
+    redirect_back(fallback_location: root_path)
+  end
 end
