f627939d6222b8808260f32b793825eb21ea1cbc
Kota Kamiyama <k.kamikami1211@gmail.com>
2020-12-10 19:38:39 +0900

    add: リンクを連打した時の対策を追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,9 +5,15 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.build(like_param)
-    @like.save
 
-    redirect_back(fallback_location: root_path)
+    #お気に入り作成ボタンを連打した時の対策
+    if Like.exists?(user_id: @like.user_id, post_id: @like.post_id)
+      redirect_back(fallback_location: root_path)
+    else
+      @like.save
+
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
@@ -23,5 +29,14 @@ class LikesController < ApplicationController
 
   def set_like
     @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+  end
+
+  def access_limit(model)
+    #お気に入りボタン解除ボタンを連打した時の対策
+    if model == nil
+      redirect_back(fallback_location: root_path)
+    elsif current_user.id != model.user_id
+      redirect_to root_path
+    end
   end
 end
