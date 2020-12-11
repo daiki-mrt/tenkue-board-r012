63cca0b02b2414869fe3ec780fbfccd857de555c
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-12-11 15:06:21 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   
+  validates :name, presence: true
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: EMAIL_REGEX }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{8,32}\z/
+  validates :password, presence: true, format: { with: PASSWORD_REGEX }
+
   def self.guest
     user = User.find_or_create_by!(email: 'guest@example.com', name: 'ゲストユーザ') do |user|
       user.password = SecureRandom.urlsafe_base64
