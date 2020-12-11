c5181f576936ea36ee9ccf6e0bc7a055d47e2da9
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-12-11 16:03:07 +0900

    userのemailを連番に設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { "テストユーザ" }
-    email { "abc@abc.com" }
+    sequence(:email) { |n| "abc#{n}@abc.com" }
     password { "abcd1234" }
     password_confirmation { password }
   end
